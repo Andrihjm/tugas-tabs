da372a050b472e38e95ef043ca78299a64263992
Andrihjm <andriisumsel@gmail.com>
2023-10-31 12:42:08 +0700

    yay

--- script/apps.js
+++ script/apps.js
@@ -1,3 +1,19 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+
+
+btns.forEach(e => {
+    e.addEventListener('click', () => {
+        btns.forEach(btn => btn.classList.remove('active'))
+        e.classList.add('active')
+        
+        const targetId = e.getAttribute('data-id')
+        
+        articles.forEach(content => {
+            content.classList.remove('active')
+        })
+
+        document.getElementById(targetId).classList.add('active')
+    })
+})
